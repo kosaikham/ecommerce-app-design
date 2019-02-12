036fea9b1ae3897c9792f6fc693b08dff2d0cbbf
saikham <saikham.email@gmail.com>
2019-02-12 15:38:17 +0630

    - Finish Shipping page on Checkout section - Ready to go Payment page

---
+++ src/components/ShippingPartner.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { View, Text, Image } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp
+} from "react-native-responsive-screen";
+
+class ShippingPartner extends Component {
+  render() {
+    const { imageUri, name, description } = this.props;
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingVertical: 15
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "center"
+          }}
+        >
+          <View
+            style={{
+              width: wp("4.3%"),
+              height: wp("4.3%"),
+              borderWidth: 1,
+              borderColor: "gray",
+              marginRight: 10
+            }}
+          />
+          <View
+            style={{
+              width: wp("15%"),
+              height: wp("15%")
+            }}
+          >
+            <Image
+              source={imageUri}
+              style={{
+                flex: 1,
+                width: null,
+                height: null,
+                resizeMode: "cover"
+              }}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "flex-end"
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: "bold"
+            }}
+          >
+            {name}
+          </Text>
+          <Text
+            style={{
+              fontSize: 12,
+              color: "gray"
+            }}
+          >
+            {description}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default ShippingPartner;
--- src/views/Shipping.js
+++ src/views/Shipping.js
@@ -1,17 +1,148 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp
+} from "react-native-responsive-screen";
+import ShippingPartner from "../components/ShippingPartner";
 
 class Shipping extends Component {
   render() {
     return (
       <View
         style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
+          flex: 1
         }}
       >
-        <Text>Shipping</Text>
+        <View
+          style={{
+            height: hp("45%"),
+            width: "100%",
+            marginTop: 20,
+            borderBottomWidth: 1,
+            borderBottomColor: "gray"
+          }}
+        >
+          <View
+            style={{
+              paddingHorizontal: 15
+            }}
+          >
+            <Text
+              style={{
+                color: "gray",
+                fontSize: 14
+              }}
+            >
+              Which shipping partner do you like?
+            </Text>
+            <View
+              style={{
+                marginTop: 15
+              }}
+            >
+              <ShippingPartner
+                imageUri={require("../../assets/logo/logo_1.jpg")}
+                name="DHL"
+                description="No additional Costs"
+              />
+              <ShippingPartner
+                imageUri={require("../../assets/logo/logo_2.jpg")}
+                name="UPS"
+                description="No additional Costs"
+              />
+              <ShippingPartner
+                imageUri={require("../../assets/logo/logo_3.png")}
+                name="FEDEX EXPRESS"
+                description="Additional 12.99 $"
+              />
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            paddingVertical: 15,
+            flex: 1,
+            paddingHorizontal: 15
+          }}
+        >
+          <Text
+            style={{
+              color: "gray",
+              fontSize: 14,
+              marginBottom: 15
+            }}
+          >
+            Shipping Address
+          </Text>
+          <Text
+            style={{
+              fontSize: 14
+            }}
+          >
+            Johnny Doe
+          </Text>
+          <Text
+            style={{
+              fontSize: 14
+            }}
+          >
+            11144 Military Trail (North)
+          </Text>
+          <Text
+            style={{
+              fontSize: 14
+            }}
+          >
+            Apartment #3122
+          </Text>
+          <Text
+            style={{
+              fontSize: 14
+            }}
+          >
+            23122 Palo Alot
+          </Text>
+          <Text
+            style={{
+              fontSize: 14
+            }}
+          >
+            California, United States
+          </Text>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "flex-end"
+            }}
+          >
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={() => this.props.navigation.navigate("Payment")}
+              style={{
+                backgroundColor: "#F08C4F",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: 2,
+                shadowOffset: { width: 1, height: 2 },
+                shadowColor: "#000",
+                shadowOpacity: 0.4,
+                elevation: 4,
+                paddingVertical: 10
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontWeight: "500",
+                  color: "white"
+                }}
+              >
+                Next Step
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
